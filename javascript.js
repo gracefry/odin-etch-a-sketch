1639df0df308c937e603eb76982777671b4abf40
Grace <99232016+gracefry@users.noreply.github.com>
2022-12-21 23:00:19 +1100

    Add hover and button functionality
    * Background colour changes on `mouseover`
    * Button exists and asks for grid size
    * Prompt has naive error handling
    * Button press clears existing div and makes new grid of given size

--- javascript.js
+++ javascript.js
@@ -1,27 +1,74 @@
-const grid = document.querySelector("#grid");
+let size = 16; // Set grid size
+makeGrid(); // Make initial grid
 
-// Create row divs
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.id = `${i + 1}`
+// Add button functionality
+const button = document.querySelector("button");
 
-    // Style
-    row.style.display = "flex";
-    // row.style.border = "1px solid yellow";
-    
-    grid.appendChild(row);
+button.addEventListener("click", (e) => {
+    let n = prompt("Provide grid size: \n", 16);
 
-    // Create cells to rows
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div")
-        cell.id = `x${i + 1}-y${j + 1}`;
-        cell.classList.add("cell");
-
-        // Style
-        cell.style.flex = "1";
-        // cell.style.border = "1px solid red";  
-
-        row.appendChild(cell)
+    if (isNaN(n)) {
+        alert("Size must be a number")
+    } else if (n % 1 != 0) {
+        alert("Size must be an integer")
+    } else if (n >= 100) {
+        alert("Size must be under 100")
+    } else if (n <= 0) {
+        alert("Size must be above 0")
+    } else {
+        size = n;
+        clearDiv();
+        makeGrid();
+        addHover();
     }
+
+});
+
+function makeGrid() {
+    const grid = document.querySelector("#grid");
+
+    // Create row divs
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.id = `${i + 1}`
+    
+        // Style
+        row.style.display = "flex";
+        
+        grid.appendChild(row);
+    
+        // Create cells to rows
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div")
+            cell.id = `x${i + 1}-y${j + 1}`;
+            cell.classList.add("cell");
+    
+            // Style
+            cell.style.flex = "1";
+            cell.style.aspectRatio = "1 / 1"
+            cell.style.border = "1px solid #eee";  
+    
+            row.appendChild(cell)
+        }
+    }
+
+    addHover();
+}
+
+// Change color on hover
+function addHover() {
+    const cells = document.querySelectorAll(".cell") 
+
+    cells.forEach(cell => cell.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "purple";
+    }));
+}
+
+function clearDiv() {
+    const grid = document.querySelector("#grid");
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    } 
 }
